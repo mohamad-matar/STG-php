63e7319056cd5819807a20ee4b555977d1a86630
unknown <mtr9851@gmail.com>
2025-05-28 23:21:28 +0300

    dashboard-routes-fix2

--- routes/admin.php
+++ routes/admin.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/admin-dashboard', function () {
+Route::get('/dashboard-admin', function () {
 return view('admin.dashboard');
-})->middleware(['auth', 'verified' , 'user-type:admin'])->name('admin-dashboard');
+})->middleware(['auth', 'verified' , 'user-type:admin'])->name('dashboard-admin');
--- routes/provider.php
+++ routes/provider.php
@@ -1,6 +1,6 @@
 <?php
 use Illuminate\Support\Facades\Route;
 
-Route::get('/provider-dashboard', function () {
+Route::get('/dashboard-provider', function () {
     return view('provider.dashboard');
-})->middleware(['auth', 'verified', 'user-type:provider'])->name('provider-dashboard');
+})->middleware(['auth', 'verified', 'user-type:provider'])->name('dashboard-provider');
