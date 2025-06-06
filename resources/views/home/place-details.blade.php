e6aaeaaeacc87aa243a3897aaf06ea376d7241d4
unknown <mtr9851@gmail.com>
2025-06-06 13:38:00 +0300

    tourist-search-details

--- resources/views/home/place-details.blade.php
+++ resources/views/home/place-details.blade.php
@@ -1,14 +1,21 @@
 @extends('layouts.master')
 @section('content')
-    <div class="container my-5">
-        <div class="container">
-            <img src="{{ getImgUrl($place->image_id) }}" alt="">
+    <div class="my-5">
+        <div class="p-2 my-3">
+            <h1 class="text-center text-success">{{ $place->name_ar }}</h1>
+            <p class="text-center fs-3">{{ $place->description_ar }}</p>
+            <div class="main-img-wrapper">
+                <img src="{{ getImgUrl($place->image_id) }}" alt="">
+            </div>
         </div>
-
-        <div class="container" id="results">
-            @foreach ($place->placeshows as $placeshow)
-                <img src="{{ getImgUrl($placeshow->image_id) }}" alt="">
-            @endforeach
+        <div>
+            <h3 class="text-center my-3">الصور التفصيلية</h3>
+            <div class="container" id="results">
+                @foreach ($place->placeshows as $placeshow)
+                    <img onmouseover="viewName('{{ $placeshow->name_ar }}')"  src="{{ getImgUrl($placeshow->image_id) }}" alt="">
+                @endforeach
+            </div>
+            <p id="view-name" class="fs-5 text-center py-2"></p>
         </div>
     </div>
 @endsection
@@ -16,4 +23,11 @@
     <link rel="stylesheet" href="{{ asset('assets/unpkg/custom.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/unpkg/unpkg-normalize.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/unpkg/custom.css') }}">
+@endpush
+@push('js')
+    <script>
+        function viewName(name) {
+            document.getElementById('view-name').innerText = name;
+        }
+    </script>
 @endpush
