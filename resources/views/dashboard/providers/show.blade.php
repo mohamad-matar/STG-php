1bab31d2ba0daa93e82404e661bfaf4626b5284e
hanankassis <kassishanan83@gmail.com>
2025-06-08 08:46:26 +0300

    provider show

---
+++ resources/views/dashboard/providers/show.blade.php
@@ -0,0 +1,96 @@
+@extends('layouts-dashboard.master')
+@section('content')
+    <div class="d-flex justify-content-between align-items-center mb-2">
+        <h2> إدارة صور {{ $provider->name_ar }}</h2>
+        <div>
+            <button type="button" onclick="showForm('store' , {{ $provider->id }}, '' ,'' ,'')" 
+                class="btn btn-secondary">
+                إضافة صورة
+            </button>
+            <a class="btn btn-secondary" href="{{ route('admin.providers.edit') }}">عودة</a>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-9">
+
+                @if ($provider->providerShows->isEmpty())
+                    <div class="text-center mb-5">
+                        <img src="{{ asset('assets/images/no-data.jpg') }}" alt="" class="w-50 rounded rounded-5">
+                    </div>
+                @else
+                    <table class="table table-bordered table-striped table-hover table-condensed">
+                        <tr class="table-secondary">
+                            <th> الوصف بالعربي</th>
+                            <th> الوصف بالانكليزي</th>
+                            <th> الصورة </th>
+                            <th> الوظائف</th>
+                        </tr>
+
+                        @foreach ($provider->providerShows as $providerShow)
+                            <tr>
+                                <td>{{ $providerShow->name_ar }}</td>
+                                <td>{{ $providerShow->name_en }}</td>
+                                <td class="text-center">
+                                    <img id="img-review" src="{{ getImgUrl($providerShow->image_id) }}" alt=""
+                                        width="100" height="75" onclick="openLightbox(this)">
+                                </td>
+                                <td class="text-nowrap text-center">
+                                    <button type="button"
+                                        onclick="showForm('update' , {{ $provider->id }}, {{ $providerShow->id }} , '{{ $providerShow->name_ar }}' , '{{ $providerShow->name_en }}' )"
+                                        class="btn btn-sm btn-outline-info">
+                                        <i data-feather="edit"></i>
+                                    </button>
+                                    <form action="{{ route('admin.providershows.destroy', $providerShow) }}" method="post"
+                                        class="d-inline-block"
+                                        onsubmit="return  confirm('Are you sure to delete {{ $providerShow->name_ar }}')">
+                                        @csrf
+                                        @method('delete')
+                                        <button class="btn btn-sm btn-outline-danger"><i data-feather="trash"></i></button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+
+                    </table>                    
+                @endif
+                <x-show-image />
+
+            </div>
+            <div class="col-3">
+                <div id="form-create-container"></div>
+            </div>
+        </div>
+    </div>
+@endsection
+@push('js')
+
+    <script>
+        function showForm(type, provider, providerShow, name_ar, name_en) {
+            const method = type == 'update' ? `@method('put')` : '';
+            const typeInAr = type == 'store' ? 'إنشاء' : 'تعديل';
+
+            const uploadFileElement = `<x-image-upload name='image_id' label='الصورة الرئيسية' col='12'/>`;
+
+            const form = `
+            <form action="/admin/providerShows/${providerShow}" method="post" enctype="multipart/form-data" class="bg-secondary p-3 text-white">
+                @csrf
+                ${method}
+                <div class="row">
+            
+                <input type="hidden" name="provider_id" value="${provider}">
+
+                <x-input name="name_ar" label="الاسم بالعربي" col="12" dbValue=${name_ar} />
+                <x-input name="name_en" label="الاسم بالانكليزي" col="12" dbValue=${name_en} />
+                
+
+                ${type == 'store'? uploadFileElement : ''}
+                <div>
+                    <button class="btn btn-dark"> ${typeInAr} الصورة</button>
+                    <a href="{{ URL::current() }}" class="btn btn-outline-dark">رجوع</a>
+                </div>
+            </form>`;
+            document.getElementById('form-create-container').innerHTML = form;
+        }        
+    </script>
+@endpush
--- resources/views/layouts-dashboard/sidebar.blade.php
+++ resources/views/layouts-dashboard/sidebar.blade.php
@@ -1,51 +1,63 @@
 <nav id="sidebar" class="sidebar js-sidebar">
     <div class="sidebar-content js-simplebar">
         <a class="sidebar-brand" href="{{ route('dashboard') }}">
-            <span class="align-middle">@lang('stg.' . config('app.name', 'stg'))</span>
+            <span class="fs-2 fw-normal">الدليل السياحي السوري</span>
         </a>
 
-        <ul class="sidebar-nav">
+        <ul class="sidebar-nav fs-4 text-end">
             <li class="sidebar-header">
-                {{ auth()->user()->email }}
+                <div class="fs-2">
+                    @if (auth()->user()->type == 'admin')
+                    المدير العام للموقع
+                    @elseif (auth()->user()->type == 'provider')
+                    {{ auth()->user()->provider->name_ar }}
+                    @endif
+                </div>
             </li>
 
             @if (auth()->user()->type == 'admin')
                 <li class="sidebar-item @if (str_contains(Route::currentRouteName(), 'services')) active @endif">
                     <a class="sidebar-link" href="{{ route('admin.services.index') }}">
-                        <i class="align-middle" data-feather="activity"></i>
                         <span class="align-middle">services</span>
+                        <i class="align-middle" data-feather="activity"></i>
                     </a>
                 </li>
                 <li class="sidebar-item @if (str_contains(Route::currentRouteName(), 'categories')) active @endif">
                     <a class="sidebar-link" href="{{ route('admin.categories.index') }}">
-                        <i class="align-middle" data-feather="server"></i>
                         <span class="align-middle">categories</span>
+                        <i class="align-middle" data-feather="server"></i>
                     </a>
                 </li>
                 <li class="sidebar-item @if (str_contains(Route::currentRouteName(), 'places')) active @endif">
                     <a class="sidebar-link" href="{{ route('admin.places.index') }}">
-                        <i class="align-middle" data-feather="aperture"></i>
                         <span class="align-middle">places</span>
+                        <i class="align-middle" data-feather="aperture"></i>
                     </a>
                 </li>
                 <li class="sidebar-item @if (str_contains(Route::currentRouteName(), 'providers')) active @endif">
-                    <a class="sidebar-link" href="{{ route('admin.providers.index' , ['accepted' => 0]) }}">
+                    <a class="sidebar-link" href="{{ route('admin.providers.index', ['accepted' => 0]) }}">
+                        <span class="align-middle">مزودي الخدمة المعلقين </span>
                         <i class="align-middle" data-feather="alert-circle"></i>
-                        <span class="align-middle">Pinned provider</span>
                     </a>
                 </li>
 
             @elseif (auth()->user()->type == 'provider')
                 <li class="sidebar-item @if (Route::currentRouteName() == 'provider.edit') ) active @endif">
                     <a class="sidebar-link" href="{{ route('provider.edit') }}">
+                        <span class="align-middle">الإعدادت </span>
                         <i class="align-middle" data-feather="sliders"></i>
-                        <span class="align-middle">Settings</span>
+                    </a>
+                </li>
+                <li class="sidebar-item @if (Route::currentRouteName() == 'provider.show') ) active @endif">
+                    <a class="sidebar-link" href="{{ route('provider.show') }}">
+                        <span class="align-middle"> إعداد الصور </span>
+                        <i class="align-middle" data-feather="image"></i>
                     </a>
                 </li>
                 <li class="sidebar-item @if (str_contains(Route::currentRouteName(), 'places')) active @endif">
                     <a class="sidebar-link" href="{{ route('provider.places.create') }}">
+                        <span class="align-middle"> إنشاء مكان</span>
                         <i class="align-middle" data-feather="aperture"></i>
-                        <span class="align-middle">Create Place</span>
                     </a>
                 </li>
             @endif
